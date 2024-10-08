80fb1669ed565b9a709c5132119e8ba6366909f7
maxt41 <maxhthomas@outlook.com>
2024-10-08 19:09:46 +0100

    Github actions test

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -7,14 +7,14 @@ notes.get('/:id', (c) => {
 
     if (isNaN(id)) {
         return c.json({ error: 'Provided ID was not an integer' })
-    }
+    };
 
-    return c.json({ note: `Note ID: ${id}` }, 200)
+    return c.json({ note: `Note ID: ${id}` }, 200);
 });
 
 notes.get('/', (c) => {
-    const notes = [{ id: 1, note: 'Hello world!' }, { id: 2, note: 'Goodbye world?' }]
-    return c.json({ notes: notes })
+    const notes = [{ id: 1, note: 'Hello world!' }, { id: 2, note: 'Goodbye world?' }];
+    return c.json({ notes: notes });
 });
 
 export default notes;
